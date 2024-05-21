5bf002199394898fd25cc03349ae144043a37f9f
d-kudin <97442072+d-kudin@users.noreply.github.com>
2024-05-21 22:56:42 +0200

    Update mydb.py

--- mydb.py
+++ mydb.py
@@ -3,7 +3,7 @@ import mysql.connector
 dataBase = mysql.connector.connect(
 	host = 'localhost',
 	user = 'root', #User from MySQL
-	passwd = '1234' #Password from MySQL
+	passwd = 'Pass123aeh@@' #Password from MySQL
 	)
 
 # prepare a cursor object
@@ -11,3 +11,5 @@ cursorObject = dataBase.cursor()
 
 # Create a database
 cursorObject.execute("CREATE DATABASE Baza_Danych")
+
+print("Udalo sie")
