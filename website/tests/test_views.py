871ca0bc43cc9fbdd3178b6914056300dbf1c8e5
d-kudin <97442072+d-kudin@users.noreply.github.com>
2024-05-31 20:47:41 +0200

    Utworzenie test_views.py do testowania widoku "home"

---
+++ website/tests/test_views.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+class HomeViewTest(TestCase):
+    
+    def test_home_view_status_code(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_view_template_used(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'home.html')
