93e2c4d46074a61638a4a28fd8ce84fe16bd32d2
d-kudin <97442072+d-kudin@users.noreply.github.com>
2024-05-31 20:46:12 +0200

    Utworzenie test_forms.py do testowania SignUpForm

---
+++ website/tests/test_forms.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from website.forms import SignUpForm
+
+class SignUpFormTest(TestCase):
+    
+    def test_signup_form_valid_data(self):
+        form = SignUpForm(data={
+            'username': 'jan_kowalski',
+            'first_name': 'Jan', 
+            'last_name': 'Kowalski', 
+            'email': 'jan.kowalski@example.com',
+            'password1': 'SuperSecret123',
+            'password2': 'SuperSecret123'
+        })
+        print(form.errors)
+        self.assertTrue(form.is_valid())
+
+    def test_signup_form_invalid_data(self):
+        form = SignUpForm(data={})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 6)
